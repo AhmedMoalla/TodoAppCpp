6b90ecf34eff1ddadd4a3a01649d7832dac7b807
Ahmed Moalla <ahmed.moalla@novencia.com>
2024-10-07 23:13:57 +0200

    Start refacto

---
+++ Tasks.UI/Repl/src/test.cpp
@@ -0,0 +1,3 @@
+//
+// Created by ahmed on 07/10/2024.
+//
--- main.cpp
+++ main.cpp
@@ -4,8 +4,28 @@
 
 void display_ui(const todo::TaskManager& task_manager);
 
+class TaskUI {
+public:
+    explicit TaskUI(todo::TaskManager* task_manager) : task_manager(task_manager) {}
+    virtual ~TaskUI() = default;
+    virtual void display() const = 0;
+protected:
+    todo::TaskManager* task_manager;
+};
+
+class NewUI : public TaskUI {
+public:
+    explicit NewUI(todo::TaskManager* task_manager)
+        : TaskUI(task_manager) {}
+
+    void display() const override {
+        std::println("Displaying new UI {}", task_manager->find_all().size());
+    }
+};
+
 int main() {
     std::println("Starting up TodoApp v{}.{} in '{}' mode", VERSION_MAJOR, VERSION_MINOR, TODO_UI_MODE);
-    const todo::TaskManager task_manager;
-    display_ui(task_manager);
+    const auto task_manager = std::make_unique<todo::TaskManager>();
+    const NewUI ui(task_manager.get());
+    ui.display();
 }
